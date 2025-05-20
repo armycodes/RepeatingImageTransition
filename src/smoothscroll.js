4405158b591bcb7490be63d8f95ea4922c96cd88
Siri Mahalaxmi Vemula <114645253+armycodes@users.noreply.github.com>
2025-05-20 15:40:07 +0530

    Fix Lenis constructor access

--- src/smoothscroll.js
+++ src/smoothscroll.js
@@ -1,8 +1,5 @@
-// Import Lenis from global scope since it's added there by the library
-const Lenis = globalThis.Lenis;
-
 export const initSmoothScrolling = () => {
-  const lenis = new Lenis({ lerp: 0.1 });
+  const lenis = new globalThis.Lenis({ lerp: 0.1 });
 
   gsap.ticker.add((time) => {
     lenis.raf(time * 1000);
