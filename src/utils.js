6152094685c7c8576e0d4cd966baede69e3f46f5
Siri Mahalaxmi Vemula <114645253+armycodes@users.noreply.github.com>
2025-05-20 15:38:44 +0530

    Updated utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import imagesLoaded from 'imagesloaded';
+
 export const preloadImages = (selector = 'img') => {
   return new Promise((resolve) => {
     imagesLoaded(document.querySelectorAll(selector), { background: true }, resolve);
